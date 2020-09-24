ec837b539dac09faad09d613e04bb2023aeb214a
shury2323 <gabrielbbrito_1@hotmail.com>
2020-09-24 00:05:52 -0300

    versao 1.5

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index-funcionario.ejs');
 });
 
 app.get('/', (req, res) => {
@@ -35,30 +35,30 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/show', (req, res) => {
+app.get('/show-funcionario', (req, res) => {
     db.collection('funcionario').find().toArray((err, results) => {
         if (err) return console.log(err)
-        res.render('show.ejs', { funcionario: results })
+        res.render('show-funcionario.ejs', { funcionario: results })
 
     })
 })
 
-app.post('/show', (req, res) => {
+app.post('/show-funcionario', (req, res) => {
     db.collection('funcionario').save(req.body, (err, result) => {
         if (err) return console.log(err)
 
         console.log('Salvo no Banco de Dados')
-        res.redirect('/show')
+        res.redirect('/show-funcionario')
     })
 });
 
-app.route('/edit/:id')
+app.route('/edit-funcionario/:id')
     .get((req, res) => {
         let id = req.params.id
 
         db.collection('funcionario').find(ObjectId(id)).toArray((err, result) => {
             if (err) return res.send(err)
-            res.render('edit.ejs', { funcionario: result })
+            res.render('edit-funcionario.ejs', { funcionario: result })
         })
     })
     .post((req, res) => {
@@ -89,7 +89,7 @@ app.route('/edit/:id')
             }
         }, (err, result) => {
             if (err) return res.send(err)
-            res.redirect('/show')
+            res.redirect('/show-funcionario')
             console.log('atualizado no banco de dados')
         })
     })
@@ -100,6 +100,6 @@ app.route('/delete/:id')
         db.collection('funcionario').deleteOne({ _id: ObjectId(id) }, (err, result) => {
             if (err) return res.send(500, err)
             console.log('Deletando do banco de dados!')
-            res.redirect('/show')
+            res.redirect('/show-funcionario')
         })
     })
